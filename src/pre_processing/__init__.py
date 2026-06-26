93a7d5fc336ed9663e233ffacc022d56ee46b4f3
HuynhGiaBao06 <huynhgiabao2718@gmail.com>
2026-06-26 15:04:06 +0700

    Feat: feat target encoding

--- src/pre_processing/__init__.py
+++ src/pre_processing/__init__.py
@@ -1,4 +1,4 @@
 from .feature_engineer import FeatureCreator
-from .binning import AgeBinner
+from .ageBinner import AgeBinner
 from .capping_outlier import OutlierCapping
 from k_fold import KFoldTargetEncoder
